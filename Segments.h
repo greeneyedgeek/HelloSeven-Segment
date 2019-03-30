e0ab9a36e5c2dc9e771fb606612eae81930a2a04
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2019-03-30 14:47:21 -0400

    Create Segments.h

---
+++ Segments.h
@@ -0,0 +1,24 @@
+#ifndef SEGMENTS_H
+#define SEGMENTS_H
+
+//0b0gfedcba
+//	--a--
+//  |	  |
+//	f   b
+//	|	  |
+//	--g--
+//	|	  |
+//	e	  c
+//	| 	|
+//	--d--
+
+#define LETTER_A 0b01110111
+#define LETTER_C 0b00111001
+#define LETTER_E 0b01111001
+#define LETTER_F 0b01110001
+#define LETTER_H 0b01110110
+#define LETTER_L 0b00111000
+#define LETTER_O 0x3F
+#define NO_LETTER 0x00
+
+#endif // SEGMENTS_H
